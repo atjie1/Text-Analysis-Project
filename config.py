13d7d2ff58c323dccaea0e52b43c70cf57642d3c
atjie1 <122930813+atjie1@users.noreply.github.com>
2023-03-24 23:49:32 -0400

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-NEWSAPIKEY = "27cf3209c3f54fe7bc9e0dc8dabf271a"
-
-client_id = "OhNdbQQX4Lu1YxxL-45Xqg"
-client_secret = "c87ifAS5zYd6QacCuGW2Bwy7GBMEXw"
-username = "SillyYogurt4594"
-password = "ahS25sp+6X&wTQS"
-user_agent = "everything-everywhere"
